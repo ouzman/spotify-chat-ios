f54d726aad662faaa36ec5e35a44881bb8636395
Oguzhan Uzman <ouzmann@gmail.com>
2021-05-13 19:42:26 +0300

    initialize repository

---
+++ SpotifyChatiOS/SpotifyChatiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyChatiOSApp.swift
+//  SpotifyChatiOS
+//
+//  Created by Oguzhan Uzman on 13.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyChatiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
