93decb9b7775a0fc753eaa1c88a63fa27bf8d02f
Oguzhan Uzman <ouzmann@gmail.com>
2021-05-15 00:55:09 +0300

    login buttom redesigned

--- SpotifyChatiOS/Scene/Login/LoginView.swift
+++ SpotifyChatiOS/Scene/Login/LoginView.swift
@@ -36,7 +36,12 @@ struct LoginView: View {
         Button(action: {
             viewModel.signIn()
         }) {
-            Text("Login")
+            Image("spotify-logo")
+                .renderingMode(.template)
+                .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: -40))
+                .font(.largeTitle)
+                .foregroundColor(.white)
+            Text("Login with Spotify")
                 .frame(minWidth: 0, idealWidth: 100, maxWidth: .infinity,
                        minHeight: 0, idealHeight: 100, maxHeight: .infinity,
                        alignment: .center)
@@ -46,6 +51,7 @@ struct LoginView: View {
         .background(Color(spotifyGreen))
         .cornerRadius(25)
         .font(.system(size: 18, weight: .semibold, design: .rounded))
+        .frame(width: 250, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
     }
 }
 
